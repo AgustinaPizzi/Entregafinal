b948dcd3b9dcaaef57b81533c3fe082965903b05
AgustinaPizzi <agustina1996pizzi@gmail.com>
2023-09-04 13:07:12 -0300

    cargaimg

--- data/productos.js
+++ data/productos.js
@@ -3,38 +3,35 @@ export const productos = [
     Id: 1,
     Nombre: "Varita Harry",
     Precio: 7000,
-    Imagen:""
+    Imagen: "../img/varitaharry.jpg",
     Categoria: "varitas",
   },
   {
     Id: 2,
     Nombre: "Varita de Sauco",
     Precio: 9000,
-    Imagen:
+    Imagen: "../img/varitasauco.jpg",
     Categoria: "varitas",
   },
   {
     Id: 3,
     Nombre: "Varita Hermione Granger",
     Precio: 8530,
-    Imagen:
-    
+    Imagen: "../img/varitahermione.jpg",
     Categoria: "varitas",
   },
   {
     Id: 4,
     Nombre: "Nimbus 2000",
     Precio: 3500,
-    Imagen:
-    
+    Imagen: "../img/nimbus200.jpg",
     Categoria: "escobas",
   },
   {
     Id: 5,
     Nombre: "Nimbus 2001",
     Precio: 4000,
-    Imagen:
-      
+    Imagen: "../img/nimbus2001.jpg",
     Categoria: "escobas",
   },
 
@@ -42,64 +39,56 @@ export const productos = [
     Id: 6,
     Nombre: "Nimbus Saeta de Fuego",
     Precio: 5600,
-    Imagen:
-      
+    Imagen: "../img/fuego.jpg",
     Categoria: "escobas",
   },
   {
     Id: 7,
     Nombre: "Funko Hagrid Deluxe",
     Precio: 17000,
-    Imagen:
-      
+    Imagen: "../img/funkohagrid.jpg",
     Categoria: "funkos",
   },
   {
     Id: 8,
     Nombre: "Funko Minerva Deluxe ",
     Precio: 9000,
-    Imagen:
-
+    Imagen: "../img/funkominerva.jpg",
     Categoria: "funkos",
   },
   {
     Id: 9,
     Nombre: "Funko Sybill Trelawney",
     Precio: 8420,
-    Imagen:
-      
+    Imagen: "../img/funkoTri.jpg",
     Categoria: "funkos",
   },
   {
     Id: 10,
     Nombre: "Funko Dumbeldore Deluxe ",
     Precio: 10000,
-    Imagen:
-     
+    Imagen: "../img/funkoDumble.jpg",
     Categoria: "funkos",
   },
   {
     Id: 11,
     Nombre: "Reliquias de la muerte ",
     Precio: 2500,
-    Imagen:
-     
+    Imagen: "../img/reliquias.jpg",
     Categoria: "llaveros",
   },
   {
     Id: 12,
     Nombre: "Fenix ",
     Precio: 3000,
-    Imagen:
-      
+    Imagen: "../img/llaverofenix.jpg",
     Categoria: "llaveros",
   },
   {
     Id: 13,
     Nombre: "Escudo de Hogwarts",
     Precio: 5000,
-    Imagen:
-      
+    Imagen: "../img/escudo.jpg",
     Categoria: "llaveros",
   },
 ];
